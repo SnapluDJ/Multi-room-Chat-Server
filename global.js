da81012ea133324e0d308471d12648a108fddfc5
Dongjie <dongjielu@wustl.edu>
2015-11-01 21:15:16 -0600

    no message

---
+++ global.js
@@ -0,0 +1,11 @@
+var nickname;
+
+function login () {
+	var id = document.getElementById("nickname").value;
+	if (id) {
+		$('#login').hide();
+		nickname = id;
+	}
+}
+
+document.getElementById("loginSubmit").addEventListener("click", login, false);
